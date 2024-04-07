3b7c9a3f41bb3009656574a3ce13256e527a080d
Džana <48892214+gunnmgirl@users.noreply.github.com>
2024-04-07 23:26:07 +0200

    Adds map filters options (#1)

--- app/components/OpenMapFilters.tsx
+++ app/components/OpenMapFilters.tsx
@@ -1,6 +1,6 @@
 import { useMap } from "react-leaflet";
 import type { Layer } from "leaflet";
-import { RefObject } from "react";
+import { RefObject, useState } from "react";
 
 const OpenMapFilters = ({
   markerRef,
@@ -13,17 +13,89 @@ const OpenMapFilters = ({
 }) => {
   const map = useMap();
   const layer = markerRef || circleGroupRef || featureGroupRef;
+  const [showFilters, setShowFilters] = useState(false);
 
   const handleLayer = () => {
     if (layer?.current) {
       map.addLayer(layer?.current);
     }
   };
+
+  const handleFilterButton = () => {
+    setShowFilters(!showFilters);
+  };
+
   return (
-    <div className="z-1 relative bg-red-200 flex justify-between">
-      <button onClick={handleLayer}>Layer 1</button>
-      <button>Layer 2</button>
-      <button>Layer 3</button>
+    <div className="z-1 relative bg-red-200">
+      <button onClick={handleFilterButton}>Filter Types</button>
+      {showFilters && (
+        <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+          <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+            <div className="flex items-center ps-3">
+              <input
+                id="blue-checkbox"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                htmlFor="blue-checkbox"
+                className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Blue
+              </label>
+            </div>
+          </li>
+          <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+            <div className="flex items-center ps-3">
+              <input
+                id="red-checkbox"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                htmlFor="red-checkbox"
+                className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Red
+              </label>
+            </div>
+          </li>
+          <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+            <div className="flex items-center ps-3">
+              <input
+                id="green-checkbox"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                htmlFor="green-checkbox"
+                className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Green
+              </label>
+            </div>
+          </li>
+          <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+            <div className="flex items-center ps-3">
+              <input
+                id="yellow-checkbox"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                htmlFor="yellow-checkbox"
+                className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Yellow
+              </label>
+            </div>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
