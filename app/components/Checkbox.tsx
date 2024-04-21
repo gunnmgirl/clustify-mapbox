cafd8d6c2a14c357db4ddd93d6fd1e39fadbd639
gunnmgirl <gunnmgirl@gmail.com>
2024-04-21 22:13:08 +0200

    Uses custom checkbox component for layer filters

--- app/components/Checkbox.tsx
+++ app/components/Checkbox.tsx
@@ -1,7 +1,16 @@
 import { Controller, useFormContext } from "react-hook-form";
 
-export const Checkbox = ({ name, value }: { name: string; value: string }) => {
+export const Checkbox = ({
+  label,
+  value,
+  name,
+}: {
+  label: string;
+  value: string;
+  name: string;
+}) => {
   const { control } = useFormContext();
+  const inputValue = value;
 
   return (
     <Controller
@@ -9,13 +18,27 @@ export const Checkbox = ({ name, value }: { name: string; value: string }) => {
       name={name}
       key={name}
       defaultValue={false}
-      render={({ field: { onChange, value, ref } }) => {
+      render={(props) => {
+        const handleOnChange = (e: any) => {
+          const checked = e?.target?.checked;
+          const currentLayers = props.field.value;
+          if (checked) {
+            const newLayers = [...currentLayers, inputValue];
+            props.field.onChange(newLayers);
+          } else {
+            const newLayers = currentLayers.filter(
+              (item: string) => item !== inputValue
+            );
+            props.field.onChange(newLayers);
+          }
+        };
+
         return (
           <>
             <input
-              onChange={onChange}
-              ref={ref}
-              checked={value}
+              onChange={handleOnChange}
+              ref={props.field.ref}
+              checked={props.field.value.includes(inputValue)}
               type="checkbox"
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             />
@@ -23,7 +46,7 @@ export const Checkbox = ({ name, value }: { name: string; value: string }) => {
               htmlFor="green-checkbox"
               className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
-              Green
+              {label}
             </label>
           </>
         );
--- app/components/OpenMapLayerControl.tsx
+++ app/components/OpenMapLayerControl.tsx
@@ -32,7 +32,7 @@ const OpenMapLayerControl = ({
 
   const defaultValues = {
     layer: Object.entries(mappedLayers)
-      .filter(([key, value]) => {
+      .filter(([_, value]) => {
         if (value?.current) {
           return map.hasLayer(value.current);
         }
@@ -88,57 +88,17 @@ const OpenMapLayerControl = ({
             <ul>
               <li>
                 <div className="flex items-center ps-3">
-                  <input
-                    {...register("layer")}
-                    defaultChecked
-                    value="blue"
-                    id="blue-checkbox"
-                    type="checkbox"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                  />
-                  <label
-                    htmlFor="blue-checkbox"
-                    className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Blue
-                  </label>
+                  <Checkbox value="blue" label="Blue" name="layer" />
                 </div>
               </li>
               <li>
                 <div className="flex items-center ps-3">
-                  <input
-                    {...register("layer")}
-                    defaultChecked
-                    value="red"
-                    id="red-checkbox"
-                    type="checkbox"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                  />
-                  <label
-                    htmlFor="red-checkbox"
-                    className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Red
-                  </label>
+                  <Checkbox value="red" label="Red" name="layer" />
                 </div>
               </li>
               <li>
                 <div className="flex items-center ps-3">
-                  <input
-                    {...register("layer")}
-                    defaultChecked
-                    value="green"
-                    id="green-checkbox"
-                    type="checkbox"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                  />
-                  <label
-                    htmlFor="green-checkbox"
-                    className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Green
-                  </label>
-                  {/* <Checkbox value="green" name="layer" /> */}
+                  <Checkbox value="green" label="Green" name="layer" />
                 </div>
               </li>
             </ul>
