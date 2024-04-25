b195cfae66c67c8fa383db57bdd55c855682a03a
gunnmgirl <gunnmgirl@gmail.com>
2024-04-25 22:21:56 +0200

    Adds keys to layer markers

--- app/components/OpenMap.tsx
+++ app/components/OpenMap.tsx
@@ -14,16 +14,16 @@ import "app/styles/openMap.css";
 
 const layerOne = [51.505, -0.09] as LatLngExpression;
 
-const geojson: { coordinates: LatLngExpression; type: string }[] = [
-  { coordinates: [51.505, -0.09], type: "red" },
-  { coordinates: [53, -1], type: "red" },
-  { coordinates: [49, 1], type: "red" },
-  { coordinates: [60, -4], type: "green" },
-  { coordinates: [55, -3], type: "green" },
-  { coordinates: [64, -2], type: "green" },
-  { coordinates: [40, 3], type: "blue" },
-  { coordinates: [45, 2], type: "blue" },
-  { coordinates: [46, 4], type: "blue" },
+const geojson: { coordinates: LatLngExpression; type: string; id: number }[] = [
+  { coordinates: [51.505, -0.09], type: "red", id: 1 },
+  { coordinates: [53, -1], type: "red", id: 2 },
+  { coordinates: [49, 1], type: "red", id: 3 },
+  { coordinates: [60, -4], type: "green", id: 4 },
+  { coordinates: [55, -3], type: "green", id: 5 },
+  { coordinates: [64, -2], type: "green", id: 6 },
+  { coordinates: [40, 3], type: "blue", id: 7 },
+  { coordinates: [45, 2], type: "blue", id: 8 },
+  { coordinates: [46, 4], type: "blue", id: 9 },
 ];
 
 const data = {
@@ -50,7 +50,7 @@ const OpenMap = () => {
       />
       <LayerGroup ref={red}>
         {data.red.map((item) => (
-          <Marker position={item.coordinates}>
+          <Marker key={item.id} position={item.coordinates}>
             <Popup>
               <div className={`bg-[${item.type}]`}>{item.type}</div>
             </Popup>
@@ -59,7 +59,7 @@ const OpenMap = () => {
       </LayerGroup>
       <LayerGroup ref={green}>
         {data.green.map((item) => (
-          <Marker position={item.coordinates}>
+          <Marker key={item.id} position={item.coordinates}>
             <Popup>
               <div className={`bg-[${item.type}]`}>{item.type}</div>
             </Popup>
@@ -68,7 +68,7 @@ const OpenMap = () => {
       </LayerGroup>
       <LayerGroup ref={blue}>
         {data.blue.map((item) => (
-          <Marker position={item.coordinates}>
+          <Marker key={item.id} position={item.coordinates}>
             <Popup>
               <div className={`bg-[${item.type}]`}>{item.type}</div>
             </Popup>
