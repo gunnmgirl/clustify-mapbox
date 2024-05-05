8530967e86cbddd75383b05484b58e9f0e94284c
gunnmgirl <gunnmgirl@gmail.com>
2024-05-05 23:14:24 +0200

    Adds open layers markers

--- app/components/OpenLayers.tsx
+++ app/components/OpenLayers.tsx
@@ -3,6 +3,10 @@ import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { XYZ } from "ol/source";
 import { defaults as defaultControls } from "ol/control";
+import { Vector as VectorLayer } from "ol/layer";
+import { Vector as VectorSource } from "ol/source";
+import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
+import { GeoJSON } from "ol/format";
 
 const OpenLayers = () => {
   useEffect(() => {
@@ -21,6 +25,65 @@ const OpenLayers = () => {
       }),
       controls: defaultControls(),
     });
+
+    // Add markers
+    const markers = [
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [-122.4, 37.8] },
+        properties: {
+          type: "red",
+        },
+      },
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [-110, 10] },
+        properties: {
+          type: "green",
+        },
+      },
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [-100, 10] },
+        properties: {
+          type: "red",
+        },
+      },
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [-140, 15] },
+        properties: {
+          type: "green",
+        },
+      },
+    ];
+
+    const vectorSource = new VectorSource({
+      features: new GeoJSON().readFeatures({
+        type: "FeatureCollection",
+        features: markers,
+      }),
+    });
+
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+      style: function (feature) {
+        const type = feature?.values_?.type;
+        return new Style({
+          image: new CircleStyle({
+            radius: 6,
+            fill: new Fill({ color: type === "red" ? "red" : "green" }),
+            stroke: new Stroke({
+              color: "white",
+              width: 2,
+            }),
+          }),
+        });
+      },
+    });
+
+    map.addLayer(vectorLayer);
+
     return () => {
       map.setTarget("");
     };
